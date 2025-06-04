6656bfb5eaeb1dbb4e53bdbb461a7436014014c3
Данила <75073244+kumchick2055@users.noreply.github.com>
2025-06-05 02:31:35 +0700

    init

---
+++ vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import UsersBoarder from './components/UsersBoarder.vue'
+import Button from "primevue/button"
+import Message from 'primevue/message';
+</script>
+
+<template>
+  <div class="p-4">
+    <div class="
+    flex 
+    items-center 
+    gap-4
+    font-bold text-2xl">
+      <div>
+        Учётные записи
+      </div>
+      <div> 
+        <Button>+</Button>
+      </div>
+    </div>
+    <div class="mt-2">
+      <Message severity="error" icon="pi pi-question" class="mb-2">Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</Message>
+    </div>
+    <UsersBoarder/>
+  </div>
+</template>
---
+++ vue-project/src/components/UsersBoarder.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import Password from 'primevue/password';
+import InputText from 'primevue/inputtext';
+
+const usersList = [
+  {id: 0, label: '4', typeNote: 0, login: '123', password: '123'}
+]
+</script>
+
+<template>
+  <!-- Заголовок таблицы -->
+  <div class="mt-4 grid grid-cols-4 gap-4">
+    <div>
+      Метки
+    </div>
+    <div>
+      Тип записи
+    </div>
+    <div>
+      Логин
+    </div>
+    <div>
+      Пароль
+    </div>
+  </div>
+
+  <div
+  v-for="item in usersList"
+  :key="item.id" 
+  class="mt-4 grid grid-cols-4 gap-4">
+    <div>
+      <InputText :model-value="item.label" :maxlength="50" fluid  />
+    </div>
+    <div>
+      {{ item.typeNote }}
+    </div>
+    <div>
+      <!-- {{ item.login }} -->
+        <InputText :model-value="item.login" fluid :maxlength="100" />
+    </div>
+    <div>
+      <Password :model-value="item.password" fluid :toggle-mask="true" :feedback="false" />
+    </div>
+  </div>
+</template>
